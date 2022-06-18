8bda971b11c823993972064d81656fcbdb8202ac
RobertoCodino <codinodeian2008@gmail.com>
2022-06-18 20:32:17 -0300

    carpeta public

--- app.js
+++ app.js
@@ -15,17 +15,5 @@ app.get('/lovelace', (req,res) => res.sendFile(path.join(__dirname, 'views','lov
 app.get('/turing', (req,res) => res.sendFile(path.join(__dirname, 'views','turing.html')))
 
 
-/*
-/ -> Debe direccionar al recurso index.html.
-/babbage -> Debe direccionar al recurso babbage.html.
-/berners-lee -> Debe direccionar al recurso berners-lee.html.
-/clarke -> Debe direccionar al recurso clarke.html.
-/hamilton -> Debe direccionar al recurso hamilton.html.
-/hopper -> Debe direccionar al recurso hopper.html.
-/lovelace -> Debe direccionar al recurso lovelace.html.
-/turing -> Debe direccionar al recurso turing.html.
-*/
-
-
 /*SERVIDOR*/
 app.listen(port, () => console.log('Server runing in http://localhost:' + port))
